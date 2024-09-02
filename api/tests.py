287111e75125121fb5e839a4ed975dd18a78c202
jan_kiezun <j4n3k777@gmail.com>
2024-09-02 22:55:52 +0200

    django project init

---
+++ api/tests.py
@@ -0,0 +1,11 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class HelloWorldTests(APITestCase):
+    def test_hello_world(self):
+        url = reverse("hello_world")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {"message": "Hello, World!"})
