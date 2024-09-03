5d0c06bba64de071ab1fdbc34c9234273d154f55
jan_kiezun <j4n3k777@gmail.com>
2024-09-03 23:22:47 +0200

    init article collection and basic worker setup

---
+++ article_collection/tasks.py
@@ -0,0 +1,8 @@
+﻿from celery import shared_task
+import random as rd
+
+
+@shared_task
+def scrape_articles():
+    # print random number from 0 to 100
+    print(f"\nThe random number is: {rd.randint(0, 100):=^10}")
