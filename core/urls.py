f6bb5f63a76dbf216e5a1a5f5a5729c46709a9e3
jan_kiezun <j4n3k777@gmail.com>
2024-09-10 23:36:23 +0200

    added docs with sphinx

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    path("articles/", include("article_collection.urls")),
+    path("docs/", include("docs.urls")),
 ]
---
+++ docs/modules/views.rst
@@ -0,0 +1,8 @@
+API Views
+=========
+.. automodule:: backend.api.views
+    :members:
+..
+    add stuff from data/main
+.. automodule:: data.main
+    :members:
