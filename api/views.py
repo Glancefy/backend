8ceb819f0a8f005a2251f657f989d82aa0372a5e
jan_kiezun <j4n3k777@gmail.com>
2024-09-10 23:38:03 +0200

    updated project file tree

--- api/views.py
+++ api/views.py
@@ -4,4 +4,6 @@ from rest_framework.response import Response
 
 @api_view(["GET"])
 def hello_world(request):
+    """
+    A simple API view that returns a JSON response."""
     return Response({"message": "Hello, World!"})
---
+++ article_collection/urls.py
@@ -0,0 +1,6 @@
+﻿from django.urls import path
+from .views import hello_article_collection
+
+urlpatterns = [
+    path("hello/", hello_article_collection, name="hello_world"),
+]
