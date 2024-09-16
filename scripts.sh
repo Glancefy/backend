90b702286014a171bc5eab0e4f6520ae061b188c
jan_kiezun <j4n3k777@gmail.com>
2024-09-16 18:46:14 +0200

    redis name change

--- scripts.sh
+++ scripts.sh
@@ -56,7 +56,7 @@ if [ $# -eq 1 ]; then
             docker run --name glancefy-postgres -e POSTGRES_DB=glancefy_db -e POSTGRES_USER=glancefy_user -e POSTGRES_PASSWORD=glancefy_password -p 5432:5432 -d postgres:14
         ;;
         redis)
-            docker run -d --name redis-stack-server -p 6379:6379 redis/redis-stack-server:latest
+            docker run -d --name glancefy-redis-stack-server -p 6379:6379 redis/redis-stack-server:latest
         ;;
         celery)
             celery -A core worker -l info
